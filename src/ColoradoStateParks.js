f9332429fd6c62f1beb68b7769c0f1d84c8be6c3
gorety <wagutugorety@gmail.com>
2023-04-19 17:07:34 +0300

    assignment

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,22 @@
 import React from "react";
-import howManyParks from "./parks/howManyParks";
+import howManyParks from "./parks/howManyParks.js";
+import MesaVerde from "./parks/MesaVade.js";
+import { trees, wildlife } from "./RockyMountain.js";
 
+// src/parks/RockyMountain.js
+console.log(trees);
+// => "Aspen and Pine"
+
+wildlife();
+// => "Elk,Bighorn Sheep, Moose"
 function ColoradoStateParks() {
-  howManyParks(); // => "42 parks!"
 
-  return <h1>Colorado State Parks!</h1>;
-}
+  howManyParks(); // => "42 parks!"
+  MesaVerde(); // => "42 parks!"
+  return(
+    <div>
+   <h1>Colorado State Parks!</h1>;
+   </div>
+  )
+   }
+   export default ColoradoStateParks;
